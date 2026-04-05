d718b6998407c900aebfab1f2b44513974fa3f06
Lucas-Alexandre131 <lucasalexandredeoliveira329@gmail.com>
2026-04-05 20:53:21 -0300

    types: add user types

---
+++ src/types/userTypes.ts
@@ -0,0 +1,13 @@
+export interface UserAttributes {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+}
+
+export interface UserCreationAttributes {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+}
